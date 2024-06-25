ae1b2ae981413534dcd360bd80c03faa6a865dec
pphatdev <leatsophat02@gmail.com>
2024-06-26 00:38:43 +0700

    move stracture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { menu } from "../../data/menu.js";
+import { menu } from "../data/menu.js";
 import Container from "./Container.js";
 
 import Logo from "../assets/apple-touch-icon.png";
