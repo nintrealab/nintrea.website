4a40a59f81fa83cb0fdb645213f741aed6c10a98
pphatdev <leatsophat02@gmail.com>
2024-06-26 00:39:05 +0700

    move stracture

--- data/menu.js
+++
@@ -1,6 +0,0 @@
-export const menu = [
-    { name: 'Home', path: '/' },
-    { name: 'Blogs', path: '/blogs' },
-    { name: 'Projects', path: '/projects' },
-    { name: 'About Us', path: '/about-us' },
-]
