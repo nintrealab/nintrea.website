92f9b1b31e91e8ced36140e098ee19fea640b42a
pphatdev <leatsophat02@gmail.com>
2024-06-27 23:27:03 +0700

    new: create language switcher

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Header } from '../components/Header.js';
 import { Container } from '../components/Container.js';
+import HomeHero from '../components/HomeHero.js';
 
 const Home = () => {
-
     return (
         <div className='h-screen dark:bg-slate-950'>
             <Header/>
+            <HomeHero/>
             <Container>
-                <div>Welcome Home!</div>
+                Welcome Home
             </Container>
         </div>
     );
