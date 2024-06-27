1365710a078f833b76327447c3c5247ae2ebd7d6
pphatdev <leatsophat02@gmail.com>
2024-06-27 23:37:01 +0700

    modified: app.js

--- src/index.js
+++ src/index.js
@@ -9,28 +9,24 @@ import Projects from "./views/Projects.js";
 import "./lang/i18n.js";
 
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* Static routes */}
-                <Route path="/" exact element={<Home />} />
-
-                <Route path="/blogs" element={<Blog/>} />
-                <Route path="/blogs/:title" element={<Blog/>} />
-
-                <Route path="/projects" element={<Projects/>} />
-                <Route path="/projects/:title" element={<Projects/>} />
-
-                <Route path="/about-us" element={<About/>} />
-                <Route path="/about-us/:author" element={<About/>} />
-
-                {/* Not found page */}
-                <Route path="*" element={<>No found</>} />
-            </Routes>
-        </BrowserRouter>
-    );
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(
+    <BrowserRouter>
+        <Routes>
+            {/* Static routes */}
+            <Route path="/" exact element={<Home />} />
+
+            <Route path="/blogs" element={<Blog/>} />
+            <Route path="/blogs/:title" element={<Blog/>} />
+
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/projects/:title" element={<Projects/>} />
+
+            <Route path="/about-us" element={<About/>} />
+            <Route path="/about-us/:author" element={<About/>} />
+
+            {/* Not found page */}
+            <Route path="*" element={<>No found</>} />
+        </Routes>
+    </BrowserRouter>
+);
