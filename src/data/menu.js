a83a4d8d1a1f24d4d033620969cea65146630c94
pphatdev <leatsophat02@gmail.com>
2024-06-27 23:26:01 +0700

    modified: app.js and add translate languages

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,5 +1,12 @@
-export const menu = [
-    { name: 'Blogs', path: '/blogs' },
-    { name: 'Projects', path: '/projects' },
-    { name: 'About Us', path: '/about-us' },
-]
+export const menu = {
+    en: [
+        { name: 'Blogs', path: '/blogs' },
+        { name: 'Projects', path: '/projects' },
+        { name: 'About Us', path: '/about-us' },
+    ],
+    km: [
+        { name: 'Blogs', path: '/blogs' },
+        { name: 'Projects', path: '/projects' },
+        { name: 'អំពីពួកយើង', path: '/about-us' },
+    ]
+}
--- src/index.js
+++ src/index.js
@@ -6,25 +6,31 @@ import About from "./views/About.js";
 import Blog from "./views/Blog.js";
 import "./styles/app.css";
 import Projects from "./views/Projects.js";
+import "./lang/i18n.js";
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Static routes */}
+                <Route path="/" exact element={<Home />} />
+
+                <Route path="/blogs" element={<Blog/>} />
+                <Route path="/blogs/:title" element={<Blog/>} />
+
+                <Route path="/projects" element={<Projects/>} />
+                <Route path="/projects/:title" element={<Projects/>} />
+
+                <Route path="/about-us" element={<About/>} />
+                <Route path="/about-us/:author" element={<About/>} />
+
+                {/* Not found page */}
+                <Route path="*" element={<>No found</>} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <BrowserRouter>
-        <Routes>
-            {/* Static routes */}
-            <Route path="/" exact element={<Home />} />
-
-            <Route path="/blogs" element={<Blog/>} />
-            <Route path="/blogs/:title" element={<Blog/>} />
-
-            <Route path="/projects" element={<Projects/>} />
-            <Route path="/projects/:title" element={<Projects/>} />
-
-            <Route path="/about-us" element={<About/>} />
-            <Route path="/about-us/:author" element={<About/>} />
-
-            {/* Not found page */}
-            <Route path="*" element={<>Not found</>} />
-        </Routes>
-    </BrowserRouter>
-);
+root.render(<App/>);
