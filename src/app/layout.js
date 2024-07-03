7400f62bc9e8cff77f418d57aa7bbb882e0a7570
PPhatDev <leatsophat02@gmail.com>
2024-07-03 16:10:30 +0700

    fixed warning: metadata->viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Poppins } from "next/font/google"
 import { cn } from "@/lib/utils";
-import { defaultMeta } from "@/data/meta";
 
 const fontSans = Poppins({
     subsets: ["latin"],
@@ -10,9 +9,6 @@ const fontSans = Poppins({
     weight: ["400", "500", "600", "700"],
     variable: "--font-sans",
 })
-
-
-export const metadata = defaultMeta;
 
 
 export default function RootLayout({ children }) {
