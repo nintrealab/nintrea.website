dbcd94673c25f9c1c5099606de9a1e29a3b619b0
sithuch <chhemsithuch1999@gmail.com>
2024-07-04 17:06:00 +0700

    update notification

--- src/components/author.js
+++ src/components/author.js
@@ -14,8 +14,8 @@ export const Author =()=>{
                     alt="Picture of the author" className="rounded-full w-10 h-10 object-cover"
                 />
                 <div className="flex flex-col">
-                    <h4 className="text-xs font-bold text-gray-600 mx-3 " >@CHHEM Sithuch </h4>
-                    <Link href="#" className="text-primary text-xs mx-3 hover:underline ">@CHHEM Sithuch</Link>
+                    <h4 className="text-xs font-bold text-transparent bg-gradient-to-r from-primary via-sky-500 to-[#064073] bg-clip-text mx-1">CHHEM Sithuch </h4>
+                    <Link href="#" className="  text-xs mx-1 hover:underline text-transparent bg-gradient-to-r from-primary via-sky-500 to-[#064073] bg-clip-text">@Sithuch</Link>
                 </div>
             </div>
         </Container>
