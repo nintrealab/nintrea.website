366e10689062c8037c53498ed9d3977f3d92a896
PPhatDev <leatsophat02@gmail.com>
2024-07-10 09:26:32 +0700

    update params

--- src/app/(website)/blogs/[detail]/page.js
+++ src/app/(website)/blogs/[detail]/page.js
@@ -1,15 +1,18 @@
+"use client";
+
 import { Container } from "@/components/container";
 import { Header } from "@/components/header";
-import { ListArticles } from "../(blogs)/list-articles";
 import { Hero } from "../(blogs)/hero";
 import { ContentBackground } from "../(blogs)/content-background";
 import { PostHeader } from "./post-header";
 
 import { defaultMeta  } from "@/data/meta";
 import { Footer } from "@/components/footer";
+import { useParams } from "next/navigation";
 export const viewport = defaultMeta
 
 export const Detail = () => {
+    const { detail } = useParams()
     return (
         <>
             <ContentBackground/>
--- src/app/(website)/tags/[detail]/page.js
+++ src/app/(website)/tags/[detail]/page.js
@@ -1,3 +1,4 @@
+"use client"
 import { Container } from "@/components/container";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
@@ -5,10 +6,12 @@ import { Hero } from "./hero";
 import { ContentBackground } from "../../blogs/(blogs)/content-background";
 
 import { defaultMeta  } from "@/data/meta";
+import { useParams } from "next/navigation";
 export const viewport = defaultMeta
 
 
 export const Detail = () => {
+    const { detail } = useParams()
     return (
         <>
             <ContentBackground/>
