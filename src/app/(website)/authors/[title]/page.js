41e929d0c94c799f1f32e61e835539742d5f46d6
HalloPhat <65520537+pphatdev@users.noreply.github.com>
2024-07-10 09:48:40 +0700

    Update page.js

--- src/app/(website)/authors/[title]/page.js
+++ src/app/(website)/authors/[title]/page.js
@@ -1,19 +1,9 @@
 "use client"
-import { useParams  } from "next/navigation";
+import { useParams } from "next/navigation";
 
 export default function Author() {
     const { title } = useParams()
     return (
         <div className="flex">{title}</div>
     )
-}
-
-
-export async function generateStaticParams() {
-    // Return an array of possible title values
-    return [
-        { params: { title: 'title1' } },
-        { params: { title: 'title2' } },
-        // Add more title values as needed
-    ];
 }
