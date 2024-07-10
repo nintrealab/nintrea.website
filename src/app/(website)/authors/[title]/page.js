0c6a65f652f09bb4ba59321bd076ec0ceaebcd6e
HalloPhat <65520537+pphatdev@users.noreply.github.com>
2024-07-10 09:46:13 +0700

    Update page.js

--- src/app/(website)/authors/[title]/page.js
+++ src/app/(website)/authors/[title]/page.js
@@ -7,3 +7,13 @@ export default function Author() {
         <div className="flex">{title}</div>
     )
 }
+
+
+export async function generateStaticParams() {
+    // Return an array of possible title values
+    return [
+        { params: { title: 'title1' } },
+        { params: { title: 'title2' } },
+        // Add more title values as needed
+    ];
+}
