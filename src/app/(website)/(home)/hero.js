8202586d32e0fcb79ab0f29bdb60d21c978c2fc4
PPhatDev <leatsophat02@gmail.com>
2024-07-11 14:22:51 +0700

    update Home hero

--- src/app/(website)/(home)/hero.js
+++ src/app/(website)/(home)/hero.js
@@ -1,6 +1,7 @@
 import { Container } from "@/components/container";
 import { GridBackground } from "./background";
 import { cn } from "@/lib/utils";
+import { TypewriterEffect } from "./typewriter-effect";
 
 export const Hero = () => {
     return (
@@ -9,7 +10,8 @@ export const Hero = () => {
             <div className="absolute inset-0 bg-grid-slate-900/[0.04] bg-[bottom_1px_center] dark:bg-grid-slate-400/[0.05] dark:bg-bottom dark:border-b dark:border-slate-100/5 pointer-events-none" style={{
                 maskImage: "linear-gradient(to bottom, transparent, black)",
                 WebkitMaskImage: "linear-gradient(to bottom, transparent, black)"
-            }}></div>
+            }}/>
+
             <Container className={cn('flex flex-col w-full gap-2 max-w-5xl h-full items-center justify-center relative mx-auto pt-20 sm:pt-20')}>
                 <a className="inline-flex items-center px-3 py-1 text-sm font-medium -translate-y-5 rounded-lg bg-muted" href="/">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
@@ -23,10 +25,22 @@ export const Hero = () => {
                     </svg>
                 </a>
                 <h1 className='text-4xl font-extrabold tracking-tight text-center text-slate-900 sm:text-5xl lg:text-6xl dark:text-white'>
-                    {`Sharing knowledge and`}
-                    <span className='inline-block lg:text-7xl text-transparent bg-gradient-to-r from-primary via-green-500 to-[#064073] bg-clip-text'>
-                        {` experience`}
-                    </span>
+                    <TypewriterEffect words={[
+                        {
+                            text: "Sharing ",
+                        },
+                        {
+                            text: "Knowledge ",
+                            className: "text-blue-500 dark:text-blue-500",
+                        },
+                        {
+                            text: "and",
+                        },
+                        {
+                            text: "Experience. ",
+                            className: "text-blue-500 dark:text-blue-500",
+                        },
+                    ]} />
                 </h1>
                 <p className="max-w-3xl mx-auto mt-6 text-lg text-center text-slate-600 dark:text-slate-400">
                     {`Compiled with various documents to support the study of students or those who like to research documents on the Internet.`}
