d91c4371c1ecbcac86ddac5c42436da6b60e88a6
PPhatDev <leatsophat02@gmail.com>
2024-07-13 08:22:30 +0700

    new screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
             padding: "2rem",
             screens: {
                 "2xl": "1400px",
+                "xs": "500px",
             },
         },
         extend: {
