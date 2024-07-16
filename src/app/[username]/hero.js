152b3579575bbf0976d3ee096ac266418339d7a3
PPhatDev <leatsophat02@gmail.com>
2024-07-16 14:14:52 +0700

    update author list

--- src/app/[username]/hero.js
+++ src/app/[username]/hero.js
@@ -18,28 +18,33 @@ export const ProfileHero = ({ data }) => {
                             className=" ring-2 ring-inset ring-border size-24 sm:size-32 drop-shadow rounded-3xl"
                         />
                     </div>
-                    <div className="mx-auto sm:ml-0">
+                    <div className="flex flex-col mx-auto sm:ml-0">
+                        <p>{`@`}{ data.username }</p>
+
                         <h1 className="inline-flex items-center justify-center gap-2 text-2xl font-extrabold tracking-tight w-fit sm:text-3xl sm:justify-start text-primary">
                             <span className="relative">
                                 { data.fullname }
                                 <CheckBadgeIcon className="absolute top-1.5 -right-8 size-5 sm:size-6 text-sky-600"/>
                             </span>
                         </h1>
-                        <p className="text-center sm:text-left text-foreground/90">@{ data.username }</p>
-                        <div className="flex flex-row justify-center gap-2 mt-5 sm:justify-start">
-                            <label className="inline-flex px-2 py-1 text-xs border rounded-full">
-                                <span> {`Creator of Nintrea`} </span>
-                                <CheckIcon className="ml-1 size-4"/>
-                            </label>
-                            <label className="inline-flex px-2 py-1 text-xs border rounded-full">
-                                <span> {`Author`} </span>
-                                <CheckIcon className="ml-1 size-4"/>
-                            </label>
+
+                        <div className="inline-flex items-center gap-2 mt-1 text-center sm:text-left text-foreground/90">
+                            <div className="flex flex-row justify-center gap-2 mt-1 sm:justify-start">
+                                <label className="inline-flex px-2 py-1 text-xs border rounded-full">
+                                    <span> {`Creator of Nintrea`} </span>
+                                    <CheckIcon className="ml-1 size-4"/>
+                                </label>
+                                <label className="inline-flex px-2 py-1 text-xs border rounded-full">
+                                    <span> {`Author`} </span>
+                                    <CheckIcon className="ml-1 size-4"/>
+                                </label>
+                            </div>
                         </div>
+
                     </div>
 
                     <div className="flex flex-row justify-center gap-2 text-center sm:text-left sm:justify-start">
-                        <p>Hello! I'm Least Sophat. I'm Web Front-End Developer and UIUX Designer @nintrea @uixbase</p>
+                        <p>{`Hello! I'm Least Sophat. I'm Web Front-End Developer and UIUX Designer @nintrea @uixbase`}</p>
                     </div>
 
                     <div className="flex flex-row justify-center gap-2 sm:justify-start">
@@ -57,6 +62,7 @@ export const ProfileHero = ({ data }) => {
                             <span> {`Follow`} </span>
                             <PlusIcon className="ml-1 size-4"/>
                         </Button>
+
                     </div>
 
 
