34e8f9b04ecb5e3d7f8d9b5a127928724e02598c
sithuch <chhemsithuch1999@gmail.com>
2024-07-30 21:47:47 +0700

    card response

--- src/components/card-stack.js
+++ src/components/card-stack.js
@@ -27,16 +27,16 @@ export  function CardStack({
         newArray.unshift(newArray.pop()); // move the last element to the front
         return newArray;
       });
-    }, 3000);
+    }, 5000);
   };
  
   return (
-    <div className="relative  h-60 w-60 md:h-60 md:w-96">
+    <div className="relative max-w-5xl h-60 flex items-center justify-center">
       {cards.map((card, index) => {
         return (
           <motion.div
             key={card.id}
-            className="absolute dark:bg-black bg-white h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
+            className="absolute bg-background flex flex-col h-full justify-between w-full rounded-3xl p-4 shadow-xl border border-border dark:shadow-white/[0.05]"
             style={{
               transformOrigin: "top center",
             }}
