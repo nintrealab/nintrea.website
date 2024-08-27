1dd25317da7c8242a9f365bbcb117745a449480d
sithuch <chhemsithuch1999@gmail.com>
2024-08-27 21:51:00 +0700

    çommit

--- src/components/topic-tag.js
+++ src/components/topic-tag.js
@@ -1,27 +1,37 @@
 "use client"
 
 import Link from "next/link";
-import { Button } from "./button";
 import { Container } from "./container";
-import {  } from "../data/posts";
+
+console.log(Blogs);
 export const Topic =()=>{
+
+    // <useEffect() => {
+    //   first
+    
+    //   return () => {
+    //     second
+    //   }
+    // }, [third])
+    
     return(
         <>
         <Container className=" flex items-center justify-center max-w-5xl ">
             <div className="flex items-center justify-center max-w-5xl ">
-                <ul class="flex flex-wrap  items-center justify-center *:rounded-full *:border *:border-sky-100 *:bg-sky-50 *:px-3 *:py-1 gap-2  dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10 ">
-                   {/* <li className="hover:bg-accent hover:text-accent-foreground cursor-pointer dark:hover:bg-blue-200/20 m-1">Sales</li> */}
-                    <Link  className="flex items-center text-[10px] sm:font-medium sm:text-sm 
-                                    hover:bg-accent hover:text-accent-foreground cursor-pointer dark:hover:bg-blue-200/20 m-1" href="/tags/test-blogs"
+                
+                
+                <ul className   =" flex flex-wrap  items-center justify-center ">
+                   <li className="rounded-2xl bg-primary cursor-pointer hover:bg-primary-foreground m-1 ">
+                    <Link  className=" text-xs flex items-center" href="/tags/test-blogs"
                     >
-                        <span className="relative text-xs bg-transparent rounded-2xl sm:text-sm">
+                        <span className="text-xs rounded-2xl sm:text-sm ring-1 bg-primary-foreground hover:bg-primary/10 px-2 py-1">
                             Read more<span className="sr-only">{"SDSDSK"}</span>
                         </span>
                     </Link>
-
-
-
+                    </li>
                 </ul>
+
+
             </div>
          </Container>
         </>
