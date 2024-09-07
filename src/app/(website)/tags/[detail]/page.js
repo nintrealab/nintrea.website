6cdab69a7c30788206d255624603ce4c4c6a67e3
CThuch <108870853+sithuch@users.noreply.github.com>
2024-09-07 14:06:42 +0700

    Revert "Cthuch next dev"

--- src/app/(website)/tags/[detail]/page.js
+++ src/app/(website)/tags/[detail]/page.js
@@ -9,42 +9,63 @@ import Image from "next/image";
 import Animal from "@/assets/images/animal.webp"
 import { Button } from "@headlessui/react";
 import Link from "next/link";
-import { CardEffect } from "@/components/card-effect";
-import { samplePost } from "@/data/posts";
-import {GroupArticle} from "../../blogs/(blogs)/article";
 export const viewport = dataViewport
-const TagPost=samplePost
+
+
 export const Detail = () => {
     const { detail } = useParams()
-    const beautyPosts = TagPost.filter(TagPost => TagPost.tags.includes("beauty"));
-
-    beautyPosts.forEach(TagPost => {
-      console.log(TagPost.tags[0]); // or whatever property you want to access
-    });
     return (
         <>
-        <ContentBackground/>
-        <Hero/>
-        <Container>
-        <div className="inline-flex w-[94%] m-auto items-center justify-center gap-10 ">
-                    
-        </div>
-                <div className="my-10"></div>
-            <div className="w-[70%] gap-5 justify-center items-center m-auto">
-            <div className="grid grid-cols-3 gap-5">
-                { 
-                    Array.from(TagPost).map( (post, key)=> {
-                        return (
-                            <CardEffect key={key}>
-                                <GroupArticle data={post}/>
-                            </CardEffect>
-                        )
-                    })    
-                }
-            </div>
-            </div>
-        </Container>
+            <ContentBackground/>
+            <Hero/>
+
+            <Container>
+                <div className="inline-flex w-[94%] m-auto items-center justify-center gap-10 ">
+                    <h1>Tags {'Sale'}</h1> <ul className="w-[80%] h-[1px] m-auto items-center  bg-gray-300 "></ul>
+                    </div>
+                
+                <div className="w-[70%] gap-5 justify-center items-center m-auto">
+                    <div className="grid grid-cols-3 gap-5">
+
+                        <div className="grid grid-cols-1 p-6 shadow-sm shadow-gray-200 ring-1 ring-gray-300 hover:bg-gray-200/50 bg-gray-50 rounded-2xl ">
+                            <Image className=" rounded-md " src={Animal} width={500} height={500}></Image>
+                            <div>
+                                <span className="mt-10 text-gray-600 text-sm">This Tags Created for all of you can share your experient and idea for next generation To develop code to easy AmmZemms.</span>
+                                <Link className="flex flex-end text-sm text-bold" href="/tags/test-blogs"><span className="sr-only">See More...</span> </Link>
+                            </div>
+                        </div>
+
+
+                        <div className="grid grid-cols-1 p-6 shadow-sm shadow-gray-200 ring-1 ring-gray-300 hover:bg-gray-200/50 bg-gray-50 rounded-2xl ">
+                            <Image className=" rounded-md " src={Animal} width={500} height={500}></Image>
+                            <div>
+                                <span className="mt-10 text-gray-600 text-sm">This Tags Created for all of you can share your experient and idea for next generation To develop code to easy AmmZemms.</span>
+                                <Link className="flex flex-end text-sm text-bold" href="/tags/test-blogs"><span className="sr-only">See More...</span> </Link>
+                            </div>
+                        </div>
+
+
+                        <div className="grid grid-cols-1 p-6 shadow-sm shadow-gray-200 ring-1 ring-gray-300 hover:bg-gray-200/50 bg-gray-50 rounded-2xl ">
+                            <Image className=" rounded-md " src={Animal} width={500} height={500}></Image>
+                            <div>
+                                <span className="mt-10 text-gray-600 text-sm">This Tags Created for all of you can share your experient and idea for next generation To develop code to easy AmmZemms.</span>
+                                <Link className="flex flex-end text-sm text-bold" href="/tags/test-blogs">
+                                <span className="relative text-xs bg-transparent rounded-2xl sm:text-sm">
+                                    Read more<span className="sr-only">{"SDSDSK"}</span>
+                                </span>
+                                
+                                
+                                </Link>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+                <Footer/>
+            </Container>
         </>
     );
 }
+
+
 export default Detail;
