63b17e680a0d83c2cf458be1e8bd706d7d919f49
pphatdev <leatsophat02@gmail.com>
2024-10-02 12:58:38 +0700

    📂: Update Post Header

--- src/app/(website)/blogs/[detail]/post-header.js
+++ src/app/(website)/blogs/[detail]/post-header.js
@@ -1,3 +1,4 @@
+import { BreadcrumbPost } from "@/lib/code/breadcrumb";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,9 +8,20 @@ export const PostHeader = ({ data }) => {
         <>
             <div className="relative mt-5 sm:mt-24">
                 <div>
+                    {/* <BreadcrumbPost breadcrumbs={data?.breadcrumbs || []}/> */}
+                    <ul className="flex items-center justify-start w-full gap-4 mt-3">
+                        {data?.breadcrumbs?.map((tag, key) => {
+                            return <li key={key} >
+                                <Link href="#" className="relative text-sm cursor-pointer text-primary">
+                                    {tag}
+                                    <span className="absolute inset-0 -left-1 w-[calc(100%_+1rem)] h-[calc(100%_+0.5rem)] -m-1 transition-all rounded-md hover:bg-foreground/10"></span>
+                                </Link>
+                            </li>
+                        })}
+                    </ul>
                     {/* Date */}
+                    <p date={data?.date}>{data?.date}</p>
 
-                    <datetime date={data?.date}>{data?.date}</datetime>
                     {/* Tags */}
                     <ul className="flex items-center justify-start w-full gap-4 mt-3">
                         {data?.tags?.map((tag, key) => {
@@ -36,7 +48,7 @@ export const PostHeader = ({ data }) => {
                                         alt="Picture of the author" className="object-cover rounded-full w-9 aspect-square sm:w-12"
                                     />
                                     <div className="ml-2 sm:ml-4">
-                                        <p className="font-semibold leading-5 sm:leading-6">{ author?.name }</p>
+                                        <p className="font-semibold leading-5 sm:leading-6">{ author?.title }</p>
                                         <Link href="#" className="text-sm leading-4 line-clamp-1 sm:text-base text-sky-500">{`@`}{ author?.username }</Link>
                                     </div>
                                 </div>
